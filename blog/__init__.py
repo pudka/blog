714774ac9c41ae7360c9297383e4a3917d218cdf
pudka <pudgeinvonyx@gmail.com>
2021-02-27 01:43:00 +0200

    New instance for blog application

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_babel import Babel
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,6 +10,7 @@ babel = Babel(app)
 app.config['SECRET_KEY'] = '78e1ff03db3c3a1b68af11fa9aae6dcebe9a6925d51c032d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from blog import routes
