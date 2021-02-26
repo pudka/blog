3bee5c7f4ee1c2a9177ff4b3c73f092116cbd279
pudka <pudgeinvonyx@gmail.com>
2021-02-27 01:41:23 +0200

    Logic rules

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, \
+    ValidationError
+from blog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -15,11 +17,25 @@ class RegistrationForm(FlaskForm):
                                                  EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. \
+            Please choose a different one.')
+
+    def validate_email(self, email):
+        email_exist = User.query.filter_by(email=email.data).first()
+        if email_exist:
+            raise ValidationError('That email is taken. \
+            Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
     """ Login user's."""
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+                        validators=[DataRequired(), Email()],
+                        render_kw={"placeholder": "Email address"})
+    password = PasswordField('Password', validators=[DataRequired()],
+                             render_kw={"placeholder": "Password"})
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    submit = SubmitField('Sign in')
